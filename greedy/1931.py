9c4d8337210369ee09e793231593f13798830821
mingi <kimmingi722@gmail.com>
2021-04-05 07:13:53 +0900

    update 1931

--- greedy/1931.py
+++ greedy/1931.py
@@ -1,3 +1,14 @@
+# not solved
+
+#(3,5)(1,4)(2,4) 
+# 앞 숫자가 같거나 뒤 숫자가 같을때는 길이가 적은 것을 선택
+# 그냥 단순히 비교할 때는 뒤 숫자로 비교
+# 11
+# 1 4
+# 3 5
+# 0 6
+# 5 7
+
 number = int(input())
 times = []  
 count = 1
@@ -10,33 +21,20 @@ for ind, time in enumerate(times) :
         base_start = time[0]
         base_end = time[1]
     else : 
-        diff = time[1] - time[0]
-        base_diff = base_end - base_start
-        if base_start == time[0] or base_end == time[1] : 
-            if diff < base_diff : 
-                base_start = time[0]
-                base_end = time[1]
-        elif time[1] < base_end : 
-            base_start = time[0]
-            base_end = time[1]
         if base_end < time[0] : 
             base_start = time[0]
             base_end = time[1]
             count += 1
-print(count)
-#(3,5)(1,4)(2,4) 
-# 앞 숫자가 같거나 뒤 숫자가 같을때는 길이가 적은 것을 선택
-# 그냥 단순히 비교할 때는 뒤 숫자로 비교
+            if time[0] == time[1] : 
+                base_start = time[0]
+                base_end = time[1]
+                count += 1
 
-# 11
-# 1 4
-# 3 5
-# 0 6
-# 5 7
-# 3 8
-# 5 9
-# 6 10
-# 8 11
-# 8 12
-# 2 13
-# 12 14
+        elif base_end > time[0] : 
+            if base_end < time[1] : 
+                continue
+            elif base_end > time[1] : 
+                base_start = time[0]
+                base_end = time[1]
+
+print(count)
