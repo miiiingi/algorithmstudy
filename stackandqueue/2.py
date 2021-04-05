58a9b03c69072a50477959bf890a3de79938b47d
Miiiingi <kimmingi722@gmail.com>
2021-04-05 12:29:11 +0900

    update

---
+++ stackandqueue/2.py
@@ -0,0 +1,8 @@
+# 문제
+# 초 단위로 기록된 주식가격이 담긴 배열 prices가 매개변수로 주어질 때, 가격이 떨어지지 않은 기간은 몇 초인지를 구하라.
+
+def solution(prices) : 
+    answer = [] 
+    return answer
+answer = solution([1, 2, 3, 2, 3])
+print(answer)
--- stackandqueue/dailytemperature.py
+++ stackandqueue/dailytemperature.py
@@ -1,20 +1,28 @@
 # 문제
 # 매일 화씨 온도 리스트 T를 입력받아서 더 따뜻한 날씨를 위해서는 며칠을 더 기다려야 하는지를 출력하라.
+# 75 ~ 76 가는 과정과 76 ~ 73을 가는 과정을 어떻게 분리해서 알고리즘으로 나타낼 것이냐 ?
 # un solved
 import collections
 def daily(T) : 
     answer = [] 
     for ind, item in enumerate(T) : 
-        count = 1
+        remain = len(T) - 1 - ind
+        count = 0 
         for comp in T[ind+1:] : 
             if item < comp :  
-                answer.append(count)
-                break 
-            elif item > comp : 
                 count += 1
+                answer.append(count)
+                break
+            elif item > comp : 
+                if remain <= 1 :
+                    continue
+                else :
+                    count += 1
+    return answer 
 
-    pass
 answer = daily([73, 74, 75, 71, 69, 72, 76, 73])
+# [1, 1, 4, 2, 1, 1, 0, 0]
+print(answer)
 
 
                 
