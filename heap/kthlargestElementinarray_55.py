5a9eca08765b020c1f5878e0da2ffd3a66c21f96
Miiiingi <kimmingi722@gmail.com>
2021-04-12 10:51:37 +0900

    update

--- heap/kthlargestElementinarray_55.py
+++ heap/kthlargestElementinarray_55.py
@@ -1,7 +1,14 @@
 # 문제
 # 정렬되지 않은 배열에서 k번째 큰 요소를 추출하라.
-def solution(arrange) : 
-    answer = 0 
-    return answer
-answer = solution([3,2,3,1,2,4,5,5,6])
+def solution(L, x):
+    for ind in range(len(L)) :
+        if ind == 0 and x <= L[ind] : 
+            L.insert(ind, x)
+       	elif ind > 0 and L[ind-1] <= x < L[ind] : 
+            L.insert(ind, x)
+            break
+       	elif ind == len(L) - 1 and x >= L[ind] : 
+            L.insert(ind, x)                      
+    return L
+answer = solution([20, 37, 58, 72, 91], 65)
 print(answer)
