109585c22dbb4480a1cebbe1cf544208d18ae563
Miiiingi <kimmingi722@gmail.com>
2021-04-19 18:07:25 +0900

    update

---
+++ graph/2667_unsolved.py
@@ -0,0 +1,24 @@
+def dfs(i, j, dangi, count) : 
+    if i < 0 or i >= len(dangi) or j < 0 or j >= len(dangi) or dangi[i][j][0] != '1':
+        return
+    dangi[i][j][0] = '0'
+    count += 1
+    dfs(i-1, j, dangi, count)  
+    dfs(i+1, j, dangi, count)  
+    dfs(i, j-1, dangi, count)  
+    dfs(i, j+1, dangi, count)  
+    return count
+
+N = int(input())
+dangi = [[[] for _ in range(N)] for _ in range(N)]
+for iter in range(N) : 
+    for ind, iter2 in enumerate(input()) : 
+        dangi[iter][ind].append(iter2)
+for i in range(len(dangi)) :
+    for j in range(len(dangi[0])) :
+        if dangi[i][j][0] == '0' :
+            pass
+        else : 
+            count = 0 
+            a = dfs(i, j, dangi, count)
+            print(a)
