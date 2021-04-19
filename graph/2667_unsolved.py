7ac6333c6a680fb562df2782118c94fcc8f9ad53
Miiiingi <kimmingi722@gmail.com>
2021-04-19 20:56:20 +0900

    update

--- graph/2667_unsolved.py
+++ graph/2667_unsolved.py
@@ -1,16 +1,18 @@
-def dfs(i, j, dangi, count) : 
+def dfs(i, j, dangi, house) : 
     if i < 0 or i >= len(dangi) or j < 0 or j >= len(dangi) or dangi[i][j][0] != '1':
         return
     dangi[i][j][0] = '0'
-    count += 1
-    dfs(i-1, j, dangi, count)  
-    dfs(i+1, j, dangi, count)  
-    dfs(i, j-1, dangi, count)  
-    dfs(i, j+1, dangi, count)  
-    return count
+    house += 1
+    dfs(i-1, j, dangi, house)  
+    dfs(i+1, j, dangi, house)  
+    dfs(i, j-1, dangi, house)  
+    dfs(i, j+1, dangi, house)  
+    return house
 
 N = int(input())
 dangi = [[[] for _ in range(N)] for _ in range(N)]
+count = 0
+houselist = [] 
 for iter in range(N) : 
     for ind, iter2 in enumerate(input()) : 
         dangi[iter][ind].append(iter2)
@@ -19,6 +21,8 @@ for i in range(len(dangi)) :
         if dangi[i][j][0] == '0' :
             pass
         else : 
-            count = 0 
-            a = dfs(i, j, dangi, count)
-            print(a)
+            house = 0 
+            count_house = dfs(i, j, dangi, house)
+            houselist.append(count_house)
+            count += 1
+print(count, houselist)
