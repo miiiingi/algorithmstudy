bdbe71fb823aac6ea6791074e61a4e0387ffefc2
Miiiingi <kimmingi722@gmail.com>
2021-04-20 13:01:01 +0900

    update

--- graph/monthlychallenge_4_unsolved.py
+++ graph/monthlychallenge_4_unsolved.py
@@ -1,28 +1,27 @@
 # 1. 그래프로 나타내기
 # 2. 어떻게 탐색하면서 경우를 종료시킬 건지 생각해보기, 순서는 상관없나 ? , 어떻게 순서를 배당할 것 인가?
-def dfs(a, v, graph) : 
-    count = 0 
-    candidate = [] 
-    for w in graph[v] : 
-        candidate.append(w)
-        if a[w] == 0 : 
+def dfs(start, a, graph, discovered = [], count = 0) : 
+    discovered.append(start)
+    for w in graph[start] : 
+        if a[w] == 0 :  
             continue
         else : 
-            condition_nonzero = True 
-            while condition_nonzero : 
-                if a[v] != 0 and a[w] == 0 and a[candidate[-1]] == 0 : 
-                    pass
-                if a[v] == 0 or a[w] == 0 :
-                    break
-                if a[v] < 0 :
-                    a[v] += 1
-                    a[w] -= 1
-                    count += 1
-                elif a[v] > 0 :
-                    a[v] -= 1
-                    a[w] += 1
-                    count += 1
-    return count
+            if w not in discovered : 
+                while True : 
+                    if a[w] < 0 : 
+                        a[w] += 1
+                        a[start] -= 1
+                        count += 1
+                        if a[w] == 0 : 
+                            break
+                    elif a[w] > 0 :
+                        a[w] -= 1
+                        a[start] += 1
+                        count += 1
+                        if a[w] == 0 : 
+                            break
+                discovered, count = dfs(w, a, graph, discovered, count)
+    return discovered, count
 
 def solution(a, edges) : 
     graph = {}
@@ -34,12 +33,12 @@ def solution(a, edges) :
             graph[edge[1]] = [] 
         graph[edge[0]].append(edge[1])
         graph[edge[1]].append(edge[0])
-    for v in graph.keys() :
-        if a[v] == 0 : 
-            continue
-        else : 
-            count_concat += dfs(a, v, graph)
-    return count_concat
+    graph = dict(sorted(graph.items(), key = lambda x : len(x[1]), reverse=True))
+    answer =  dfs(list(graph.keys())[0], a, graph) 
+    if answer[1] == 0 :
+        return -1
+    else : 
+        return answer[1]
 a = [-5, 0, 2, 1, 2]
 edges = [[0,1], [3, 4], [2, 3], [0, 3]]
 answer = solution(a, edges)
