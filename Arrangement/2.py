3670ef26dca7cebc1c62eb59aef1c73210a6a201
Miiiingi <kimmingi722@gmail.com>
2021-04-21 13:22:22 +0900

    update

--- Arrangement/2.py
+++ Arrangement/2.py
@@ -1,6 +1,23 @@
+import functools
+def comparison(x, y) : 
+    if x + y > y + x : 
+        return -1 
+    elif x + y < y + x : 
+        return 1
+    else :
+        return 0
 def solution(numbers) : 
-    print(sorted(numbers, key = lambda x : x[0], reverse=True))
+    numbers = map(str, numbers)
+    numlist = [] 
     answer =''
-    return answer 
-answer = solution([6, 10, 2])
+    for number in numbers : 
+        numlist.append(number)
+    numlist = sorted(numlist, key = functools.cmp_to_key(comparison))
+    for number in numlist : 
+        answer += number
+    if answer < '1' :
+        return '0'
+    else : 
+        return answer
+answer = solution([0,0,0,0])
 print(answer)
