5a7a651c5543d208881081820f5d5c0e2c9ddeff
Miiiingi <kimmingi722@gmail.com>
2021-04-23 14:06:27 +0900

    update

--- graph/2667.py
+++ graph/2667.py
@@ -5,7 +5,9 @@ for iter in range(N) :
     for ind, iter2 in enumerate(input()) : 
         dangi[iter][ind].append(iter2)
 def apart(dangi) :  
+    count_dangi = 0 
     def dfs(i, j) : 
+        nonlocal count
         if i < 0 or i >= len(dangi) or j < 0 or j >= len(dangi) or dangi[i][j][0] != '1':
             return
         dangi[i][j][0] = '0'
@@ -14,15 +16,18 @@ def apart(dangi) :
         dfs(i+1, j)
         dfs(i, j-1)
         dfs(i, j+1)
+
     for i in range(len(dangi)) :
         for j in range(len(dangi[0])) :
             if dangi[i][j][0] == '0' :
                 continue
             else : 
                 count = 0
-                # nonlocal count 
                 dfs(i, j)
+                count_dangi += 1
                 houselist.append(count)
-    return houselist
+    return sorted(houselist), count_dangi
 answer = apart(dangi)
-print(answer)
+print(answer[1])
+for y in answer[0] : 
+    print(y)
