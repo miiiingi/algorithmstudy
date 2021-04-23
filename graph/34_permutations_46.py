9654ecfe7e47424eee16de8eabf76747dcba836d
Miiiingi <kimmingi722@gmail.com>
2021-04-23 16:13:16 +0900

    update

--- graph/34_permutations_46.py
+++ graph/34_permutations_46.py
@@ -1,4 +1,18 @@
+# 재귀를 직관적으로 이해할 수 있는 사고력 기르기 !! 복습 필수 !!
 class Solution : 
     def permute(self, num) : 
-        
-        pass
+        result = [] 
+        element_prev = []
+        def dfs(elements) :  
+            if len(elements) == 0 :
+                result.append(element_prev[:])
+            for e in elements : 
+                element_next = elements[:]
+                element_next.remove(e)
+                element_prev.append(e)
+                dfs(element_next)
+                element_prev.pop()
+        dfs(num)
+        return result
+answer = Solution().permute([1,2,3])
+print(answer)
