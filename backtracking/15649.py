f1044cd8102288af244b80ed4c73f07840025406
Miiiingi <kimmingi722@gmail.com>
2021-04-23 14:52:40 +0900

    update

---
+++ backtracking/15649.py
@@ -0,0 +1,11 @@
+N, M = map(int, input())
+result = [] 
+def dfs(index, path) : 
+    if len(path) == M : 
+        return
+    for ind in range(index+1, N+1) : 
+        for ind2 in range(ind, N+1) : 
+            result.append()
+    pass
+for x in range(1, N+1) : 
+    pass
