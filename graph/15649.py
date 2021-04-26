a7225508f0c20aa4df10128b9530f541cc33ddaa
Miiiingi <kimmingi722@gmail.com>
2021-04-26 20:10:57 +0900

    update

--- graph/15649.py
+++ graph/15649.py
@@ -1,8 +1,15 @@
-N, M = map(int, input())
+N, M = map(int, input().split())
 num = [x for x in range(1, N+1)]
 result = [] 
-def NM(num) : 
+num_prev = [] 
+def NM(num, M) : 
     def dfs(num) : 
         for n in num : 
-
-            pass
+            num_next = num[:]
+            num_next.remove(n)
+            num_prev.append(n)
+            if len(num_prev) != M :  
+                result.append(num_prev)
+            dfs(num_next)
+answer = NM(num, N)
+print(answer)
