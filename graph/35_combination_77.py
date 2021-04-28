36402d5581627c69ede860c11f46ed5ee03ca931
Miiiingi <kimmingi722@gmail.com>
2021-04-28 17:23:40 +0900

    update

--- graph/35_combination_77.py
+++ graph/35_combination_77.py
@@ -1,18 +1,14 @@
 class Solution : 
-    def combine(self, n , k) : 
+    def combine(self, N , k) : 
         result = [] 
-        num = [x for x in range(1, n+1)]
+        num = [x for x in range(1, N+1)]
         num_prev = [] 
-        def dfs(num) : 
-            if len(num) == 2 :
-                result.append(num_prev[:])
-                num_prev.pop()
-            for n in num : 
-                num_next = num[:]
-                num_next.remove(n)
-                num_prev.append(n)
-                dfs(num_next)
-        dfs(num)
+        def dfs(num, start, k) : 
+            if k == 0 :
+                result.append(num[:])
+            element = num[start:]
+
+        dfs(num, 0, k)
         return result
 answer = Solution().combine(4, 2)
 print(answer)
