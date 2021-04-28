dc03dca11358b488ce336a525617c5cfe5fe8960
Miiiingi <kimmingi722@gmail.com>
2021-04-28 17:56:29 +0900

    update

--- graph/34_permutations_46.py
+++ graph/34_permutations_46.py
@@ -1,19 +1,21 @@
 # 재귀를 직관적으로 이해할 수 있는 사고력 기르기 !! 복습 필수 !! 
 # 어떻게 이렇게 생각해내지..?
+# 함수 안에서만 존재해도 되는 지역변수는 그냥 선언 해주고, 전역변수는 self로 선언해줘서 클래스내에서 다 쓸 수 있도록 ! 좀 더 파이썬 스럽게 코드 짜는 것 연습
 class Solution : 
+    result = [] 
+    element_prev = []
     def permute(self, num) : 
-        result = [] 
-        element_prev = []
-        def dfs(elements) :  
-            if len(elements) == 0 :
-                result.append(element_prev[:])
-            for e in elements : 
-                element_next = elements[:]
-                element_next.remove(e)
-                element_prev.append(e)
-                dfs(element_next)
-                element_prev.pop()
-        dfs(num)
-        return result
+        self.dfs(num)
+        return self.result
+    def dfs(self, elements) :  
+        if len(elements) == 0 :
+            self.result.append(self.element_prev[:])
+        for e in elements : 
+            element_next = elements[:]
+            element_next.remove(e)
+            self.element_prev.append(e)
+            self.dfs(element_next)
+            self.element_prev.pop()
+
 answer = Solution().permute([1,2,3])
 print(answer)
--- graph/35_combination_77.py
+++ graph/35_combination_77.py
@@ -1,14 +1,19 @@
 class Solution : 
-    def combine(self, N , k) : 
-        result = [] 
-        num = [x for x in range(1, N+1)]
-        num_prev = [] 
-        def dfs(num, start, k) : 
-            if k == 0 :
-                result.append(num[:])
-            element = num[start:]
+    result = [] 
+    num_prev = [] 
+    def combine(self, n , k) : 
+        self.num = [x for x in range(1, n+1)]
+        self.dfs(self.num, 0, k)
+        return self.result
 
-        dfs(num, 0, k)
-        return result
+    def dfs(self, num, start, k) : 
+        if k == 0 :
+            self.result.append(self.num_prev[:])
+            return
+        for ind in range(start, len(num)) : 
+            self.num_prev.append(num[ind])
+            self.dfs(num, ind+1, k-1)
+            self.num_prev.pop()
+
 answer = Solution().combine(4, 2)
 print(answer)
