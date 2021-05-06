e1beb39ea5a1e3819c68f2687aae805e26002640
mingi <kimmingi722@gmail.com>
2021-05-07 07:00:34 +0900

    update

--- graph/1.py
+++ graph/1.py
@@ -1,5 +1,13 @@
+# 모든 숫자의 조합을 다 계산하기는 해야할 듯 ??
+# 어떻게 이것들을 반복할 것인가 ? 
 def solution(numbers, target) : 
     answer = 0 
+    def dfs(index) : 
+        if sum(numbers) == target : 
+            answer += 1
+            return
+        pass
+    dfs(0)
     return answer 
 answer = solution([1,1,1,1,1], 3)
 print(answer)
