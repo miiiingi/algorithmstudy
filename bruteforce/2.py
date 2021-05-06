ae6d3fb85f09f5a8c095301e2301548dba6b6882
Miiiingi <kimmingi722@gmail.com>
2021-05-06 21:44:50 +0900

    update

---
+++ bruteforce/2.py
@@ -0,0 +1,8 @@
+# numbers의 갯수 만큼 자릿수를 만들어가자.
+# 만들어진 각각의 수들을 반복하면서 소수인지 아닌지 판단
+# 소수를 어떻게 판단하지 ? 
+def solution(numbers) :
+    answer = 0 
+    return answer 
+answer = solution('17')
+print(answer)
