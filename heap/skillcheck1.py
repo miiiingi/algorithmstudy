965934fa7a09ba0968e1b29f49bb1e2c16a47848
Miiiingi <kimmingi722@gmail.com>
2021-05-07 16:14:10 +0900

    update

--- heap/skillcheck1.py
+++ heap/skillcheck1.py
@@ -1,3 +1,13 @@
+# 입력으로 하나씩 받아서 아이디랑 이름이랑 매치시키자.
+import collections
 def solution(record) : 
+    result = collections.defaultdict(str) 
+
+    for rec in record : 
+        _, uid, name = rec.split(' ')
+        result[uid] = name
+    print(result)
     answer = [] 
     return answer
+answer =solution(["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"])
+print(answer)
