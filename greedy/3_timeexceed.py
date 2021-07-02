4372dd83c1c90049073352f173fbf70bf66f26e1
mingi <kimmingi722@gmail.com>
2021-07-03 07:38:39 +0900

    update

--- greedy/3_timeexceed.py
+++ greedy/3_timeexceed.py
@@ -1,20 +1,36 @@
-# 자신의 뒤에꺼 보다만 크게 수를 추리자.
-# while 반복문 돌면서 다음 것보다 작은 것들 discard에 넣고
-# discard의 길이가 k가 되면 반복문 종료
-def solution(number, k) : 
-    answer = ''
-    for ind in range(len(number)) : 
-        if k == 0 :
-            answer += number[ind:]
-            return answer 
-        if len(answer) == len(number) - k : 
-            return answer
-        comp = number[ind : ind + k +1]
-        if int(number[ind]) == int(max(comp)) : 
-            answer += number[ind]
-        else : 
+def solution(number, k):
+    answer = []
+    for ind in range(len(number)):
+        if k == 0:
+            return "".join(answer) + number[ind:]
+        if len(answer) == len(number) - k:
+            return "".join(answer)
+        if number[ind] >= number[ind + 1]:
+            if not answer:
+                answer.append(number[ind])
+                continue
+            if answer[-1] < number[ind]:
+                answer.pop()
+                k -= 1
+            answer.append(number[ind])
+        else:
             k -= 1
-    if len(answer) == len(number) : 
-        return answer[:len(number) - k]
-answer = solution('4177252841', 4)
+
+
+answer = solution("99919", 1)
 print(answer)
+# def solution(number, k) : 
+#     answer = ''
+#     for ind in range(len(number)) : 
+#         if k == 0 :
+#             answer += number[ind:]
+#             return answer 
+#         if len(answer) == len(number) - k : 
+#             return answer
+#         comp = number[ind : ind + k +1]
+#         if int(number[ind]) == int(max(comp)) : 
+#             answer += number[ind]
+#         else : 
+#             k -= 1
+#     if len(answer) == len(number) : 
+#         return answer[:len(number) - k]
