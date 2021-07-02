74f2ca6542968c8b8c3c8f59cf2152b4e36ec6e1
Miiiingi <kimmingi722@gmail.com>
2021-07-02 10:24:17 +0900

    update

--- greedy/3_timeexceed.py
+++ greedy/3_timeexceed.py
@@ -1,7 +1,6 @@
 # 자신의 뒤에꺼 보다만 크게 수를 추리자.
 # while 반복문 돌면서 다음 것보다 작은 것들 discard에 넣고
 # discard의 길이가 k가 되면 반복문 종료
-import collections
 def solution(number, k) : 
     answer = ''
     for ind in range(len(number)) : 
@@ -17,5 +16,5 @@ def solution(number, k) :
             k -= 1
     if len(answer) == len(number) : 
         return answer[:len(number) - k]
-answer = solution('77777', 1)
+answer = solution('4177252841', 4)
 print(answer)
