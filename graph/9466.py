3c45b26ce64c45d98c4642a8aba4bfe4d7822656
mingi <kimmingi722@gmail.com>
2021-07-08 22:38:05 +0900

    update

--- graph/9466.py
+++ graph/9466.py
@@ -1,11 +1,31 @@
 import collections
 iter = int(input())
-for i in range(iter) : 
+for i in range(iter):
+    answer = 0 
     number_student = int(input())
-    couple = [] 
-    for x in map(int, input().split()) :
+    couple = []
+    for x in map(int, input().split()):
         couple.append(x)
     graph = collections.defaultdict(int)
-    for m, c in zip(range(number_student), couple) : 
-        graph[m+1] = c
-    
+    for m, c in zip(range(number_student), couple):
+        graph[m + 1] = c
+        numlist = [0 for _ in range(number_student)]
+
+    def dfs(node, visited):
+        if node in visited : 
+            visited.append(node)
+            return visited
+        visited.append(node)
+        visited = dfs(graph[node], visited)
+        return visited
+
+    for k in graph.keys():
+        visited = []
+        visited = dfs(k, visited)
+        if visited[-1] == k : 
+            for v in visited : 
+                numlist[v-1] = 1
+    for n in numlist : 
+        if n == 0 : 
+            answer += 1
+    print(answer)
