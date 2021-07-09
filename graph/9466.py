fb7a792018ff04bfa16b2541abee8ae4c103efa4
Miiiingi <kimmingi722@gmail.com>
2021-07-09 09:49:09 +0900

    update

---
+++ graph/9466.py
@@ -0,0 +1,11 @@
+import collections
+iter = int(input())
+for i in range(iter):
+    answer = 0 
+    number_student = int(input())
+    couple = []
+    for x in map(int, input().split()):
+        couple.append(x)
+    graph = collections.defaultdict(int)
+    for m, c in zip(range(number_student), couple) : 
+        graph[m+1] = c
