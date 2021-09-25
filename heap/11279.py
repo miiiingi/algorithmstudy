506dbc49df0907eae70373ce2aee5fa4bf51b013
Miiiingi <kimmingi722@gmail.com>
2021-09-25 16:40:16 +0900

    update

---
+++ heap/11279.py
@@ -0,0 +1,18 @@
+import heapq
+import sys
+input = sys.stdin.readline
+lists = list() 
+list_answer = list()
+N = int(input())
+for _ in range(N) : 
+    n = int(input())
+    if n == 0 :
+        if not lists :  
+            list_answer.append(0) 
+        else : 
+            pops = heapq.heappop(lists)
+            list_answer.append(-pops)
+    else : 
+        heapq.heappush(lists, -n)
+for answer in list_answer : 
+    print(answer)
