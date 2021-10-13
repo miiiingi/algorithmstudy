a9b2ac663621c14ae6b6f4639fa0c6cdd1ba909b
mingi <kimmingi722@gmail.com>
2021-10-13 19:16:16 +0900

    update

--- stackandqueue/17298.py
+++ stackandqueue/17298.py
@@ -1,22 +1,21 @@
 import sys
 import collections
+
 input = sys.stdin.readline
 N = int(input())
-array = collections.deque(map(int,input().split()))
+array = collections.deque(map(int, input().split()))
 stack = []
 answer = [-1 for _ in range(N)]
 # for n in range(N) :
 #     number = array.popleft()
-#     for x in array : 
-#         if number < x : 
+#     for x in array :
+#         if number < x :
 #             answer[n] = x
 #             break
 # print(*answer)
-for ind, elem in enumerate(array) : 
-    while stack and stack[-1] < elem : 
-        answer[ind - 1] = elem
-        break
+for ind, elem in enumerate(array):
+    while stack and stack[-1][1] < elem:
+        answer[stack[-1][0]] = elem
+        stack.pop()
     stack.append((ind, elem))
 print(*answer)
-
-
