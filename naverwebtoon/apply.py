02d60132aee9e66b4f9b4cd32820848b2cf27e9c
mingi <kimmingi722@gmail.com>
2021-10-22 13:54:20 +0900

    update

--- naverwebtoon/apply.py
+++ naverwebtoon/apply.py
@@ -1,2 +1,12 @@
 import pandas as pd
-df = pd.DataFrame({'a' : [20, 30, 40], 'b' : [100, 300, 500]})
+import seaborn as sns
+import numpy as np
+def count_missing(col) : 
+    null_vec = pd.isnull(col)
+    null_sum = np.sum(null_vec)
+    return null_sum
+def prop_missing(col) : 
+    null_sum = count_missing(col)
+    return null_sum / len(col)
+titanic = sns.load_dataset('titanic')
+print(titanic.apply(prop_missing))
