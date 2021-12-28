75eaf97e2c41d7edf32eb4a57d4d7685c9218aba
mingi <kimmingi722@gmail.com>
2021-12-28 14:40:13 +0900

    update

--- graph/15650_.py
+++ graph/15650_.py
@@ -1,20 +1,25 @@
 N, M = map(int, input().split())
 list_num = [x + 1 for x in range(N)]
 result = list()
+
+
 def dfs(init, numlist, M, visited):
     visited.append(init)
     if len(visited) == M:
         result.append(visited[:])
         return
-    numlist_ = numlist[init:]
+    numlist_ = numlist[(init - 1) :]
+    numlist_.remove(init)
     for num in numlist_:
         dfs(num, numlist_, M, visited)
         visited.pop()
+
+
 for num in list_num:
     dfs(num, list_num, M, visited=list())
-for r in result : 
-    strs = ''
-    for x in r : 
-        strs += f'{str(x)} '
+for r in result:
+    strs = ""
+    for x in r:
+        strs += f"{str(x)} "
     strs = strs.strip()
     print(strs)
