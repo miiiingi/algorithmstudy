7b7cb2bacb623c76a8626f7fa974fac3fa83c8d4
mingi <kimmingi722@gmail.com>
2022-01-24 14:04:33 +0900

    update

--- graph/PAI_13_1.py
+++ graph/PAI_13_1.py
@@ -1,30 +1,28 @@
 import collections
 import heapq
-
-
 class Solution:
     def networkDelayTime(self, times, n: int, k: int) -> int:
         graph = collections.defaultdict(list)
         for time in times:
             graph[time[0]].append([time[2], time[1]])
-        visited = [k]
-        que = list()
-        que.append([0, k])
+        visited = []
+        que = [(0, k)]
         while que:
             pops = heapq.heappop(que)
-            for g in graph[pops[1]]:
-                if not g[1] in visited:
-                    visited.append(g[1])
+            if not pops[1] in visited:
+                visited.append(pops[1])
+                # if set(visited) == set(graph.keys()):
+                #     return pops[0]
+                for g in graph[pops[1]]:
                     g[0] += pops[0]
-                    heapq.heappush(que, [g[0], g[1]])
-        print(graph)
+                    heapq.heappush(que, (g[0], g[1]))
         if set(visited) != set(graph.keys()):
             return -1
         else:
-            return max(graph.values())[0][0]
-
+            return pops[0] 
 
 sol = Solution()
-answer = sol.networkDelayTime(times=[[1, 2, 1], [2, 3, 2], [1, 3, 4]], n=3, k=1)
+# answer = sol.networkDelayTime(times=[[2,1,1],[2,3,1],[3,4,1]], n=4, k=2)
+answer = sol.networkDelayTime(times=[[1, 2, 1], [2, 1, 3]], n=2, k=2)
 # answer = sol.networkDelayTime(times = [[1,2,1]], n = 2, k = 2)
 print(answer)
